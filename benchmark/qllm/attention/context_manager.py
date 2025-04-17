3a37dd9e95bb1c15764292ae9e5a744827bd2864
SimonMen65 <jingyanm@andrew.cmu.edu>
2025-04-17 12:14:23 -0400

    try dot-topk cuda

--- benchmark/qllm/attention/context_manager.py
+++ benchmark/qllm/attention/context_manager.py
@@ -5,7 +5,7 @@ from torch.utils.cpp_extension import load
 from copy import deepcopy
 from .dot_production_attention import get_multi_stage_dot_production_attention
 import json 
-from .dat_product_topk import dot_product_topk_wrapper
+from .dot_product_topk import dot_product_topk_wrapper
 
 attention_num = 0
 
